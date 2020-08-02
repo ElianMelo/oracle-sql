861ed719ad00c20eebc69a4e2afc1d36927f10a2
ElianMelo <elianmelomorais@gmail.com>
2020-08-02 01:36:35 -0300

    Operações CRUD utilizando comandos SQL

---
+++ introducao-ao-sql-com-oracle/delete.sql
@@ -0,0 +1,10 @@
+/* Comando DELETE no SQL */
+DELETE FROM tb_produtos
+WHERE produto = '1078680';
+
+SELECT * FROM tb_produtos;
+
+DELETE FROM tb_vendedores
+WHERE matricula = '00233';
+
+SELECT * FROM tb_vendedores;
